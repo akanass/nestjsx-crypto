b7db3a273eec7a3dcfd0832462d80ba07073576d
akanass <akanass@icloud.com>
2021-10-08 13:03:20 +0200

    test(update): update tests to match to latest rxjs version

--- test/integration/hash.service.test.ts
+++ test/integration/hash.service.test.ts
@@ -9,61 +9,64 @@ let keylen: number;
 let digest: string;
 
 describe('- Integration hash.service.test.ts file', () => {
-    /**
-     * Function executed before each test
-     */
-    beforeEach(() => {
-        hashService = new HashService();
-        password = 'P3HQdR35PUQLZ5ioOrsPlxx7QWra7WQl';
-        salt = 'Kt9V3wgxrhpf8GN3';
-        iterations = 4096;
-        keylen = 24;
-        digest = 'sha256';
-    });
+  /**
+   * Function executed before each test
+   */
+  beforeEach(() => {
+    hashService = new HashService();
+    password = 'P3HQdR35PUQLZ5ioOrsPlxx7QWra7WQl';
+    salt = 'Kt9V3wgxrhpf8GN3';
+    iterations = 4096;
+    keylen = 24;
+    digest = 'sha256';
+  });
 
-    /**
-     * Function executed after each test
-     */
-    afterEach(() => {
-        hashService = undefined;
-        password = undefined;
-        salt = undefined;
-        iterations = undefined;
-        keylen = undefined;
-        digest = undefined;
-    });
+  /**
+   * Function executed after each test
+   */
+  afterEach(() => {
+    hashService = undefined;
+    password = undefined;
+    salt = undefined;
+    iterations = undefined;
+    keylen = undefined;
+    digest = undefined;
+  });
 
-    /**
-     * Test if `HashService.generate()` function returns an Observable with error if parameters are wrong
-     */
-    test('- `HashService.generate()` function must return an Observable with error if parameters are wrong', (done) => {
-        hashService.generate(undefined, undefined, undefined, undefined, undefined)
-            .subscribe(() => null, err => {
-                    expect(err.message).toBe('The "digest" argument must be of type string. Received undefined');
-                    done();
-                }
-            );
-    });
-
-    /**
-     * Test if `HashService.generate()` Observable returns Buffer
-     */
-    test('- `HashService.generate()` Observable function must return a Buffer', (done) => {
-        hashService.generate(password, salt, iterations, keylen, digest).subscribe(buffer => {
-            expect(buffer).toBeInstanceOf(Buffer);
+  /**
+   * Test if `HashService.generate()` function returns an Observable with error if parameters are wrong
+   */
+  test('- `HashService.generate()` function must return an Observable with error if parameters are wrong', (done) => {
+    hashService.generate(undefined, undefined, undefined, undefined, undefined)
+      .subscribe({
+          next: () => null,
+          error: err => {
+            expect(err.message).toBe('The "digest" argument must be of type string. Received undefined');
             done();
-        });
-    });
+          }
+        }
+      );
+  });
 
-    /**
-     * Test if `HashService.generate()` Observable returns Buffer and
-     *  his string representation is `61cac683ff27580e4c68778df5208c745b0e473172778658`
-     */
-    test('- `HashService.generate()` Observable function must return a Buffer and his string representation is ' +
-        '`61cac683ff27580e4c68778df5208c745b0e473172778658`', (done) => {
-        hashService.generate(password, salt, iterations, keylen, digest).subscribe(buffer => {
-            expect(buffer.toString('hex')).toBe('61cac683ff27580e4c68778df5208c745b0e473172778658');
-            done();
-        });
+  /**
+   * Test if `HashService.generate()` Observable returns Buffer
+   */
+  test('- `HashService.generate()` Observable function must return a Buffer', (done) => {
+    hashService.generate(password, salt, iterations, keylen, digest).subscribe(buffer => {
+      expect(buffer).toBeInstanceOf(Buffer);
+      done();
     });
+  });
+
+  /**
+   * Test if `HashService.generate()` Observable returns Buffer and
+   *  his string representation is `61cac683ff27580e4c68778df5208c745b0e473172778658`
+   */
+  test('- `HashService.generate()` Observable function must return a Buffer and his string representation is ' +
+    '`61cac683ff27580e4c68778df5208c745b0e473172778658`', (done) => {
+    hashService.generate(password, salt, iterations, keylen, digest).subscribe(buffer => {
+      expect(buffer.toString('hex')).toBe('61cac683ff27580e4c68778df5208c745b0e473172778658');
+      done();
+    });
+  });
 });
--- test/unit/aes.service.test.ts
+++ test/unit/aes.service.test.ts
@@ -7,84 +7,86 @@ let password: string;
 let salt: string;
 
 describe('- Unit aes.service.test.ts file', () => {
-    /**
-     * Function executed before each test
-     */
-    beforeEach(() => {
-        aesService = new AesService();
-        password = 'P3HQdR35PUQLZ5ioOrsPlxx7QWra7WQl';
-        salt = 'Kt9V3wgxrhpf8GN3';
-    });
+  /**
+   * Function executed before each test
+   */
+  beforeEach(() => {
+    aesService = new AesService();
+    password = 'P3HQdR35PUQLZ5ioOrsPlxx7QWra7WQl';
+    salt = 'Kt9V3wgxrhpf8GN3';
+  });
 
-    /**
-     * Function executed after each test
-     */
-    afterEach(() => {
-        aesService = undefined;
-        password = undefined;
-        salt = undefined;
-    });
+  /**
+   * Function executed after each test
+   */
+  afterEach(() => {
+    aesService = undefined;
+    password = undefined;
+    salt = undefined;
+  });
 
-    /**
-     * Test if `AesService.createKey()` function returns an Observable
-     */
-    test('- `AesService.createKey()` function must return an Observable', (done) => {
-        expect(aesService.createKey(null, null)).toBeInstanceOf(Observable);
-        done();
-    });
+  /**
+   * Test if `AesService.createKey()` function returns an Observable
+   */
+  test('- `AesService.createKey()` function must return an Observable', (done) => {
+    expect(aesService.createKey(null, null)).toBeInstanceOf(Observable);
+    done();
+  });
 
-    /**
-     * Test if `AesService.createKey()` function returns an Observable with error if AesService key parameters are wrong
-     */
-    test('- `AesService.createKey()` function must return an Observable with error if AesService key parameters are wrong', (done) => {
-        const spy = jest.spyOn(aesService[ '_aes' ], 'createKey');
-        spy.mockImplementationOnce(() => throwError(new Error('Wrong AesService key')));
+  /**
+   * Test if `AesService.createKey()` function returns an Observable with error if AesService key parameters are wrong
+   */
+  test('- `AesService.createKey()` function must return an Observable with error if AesService key parameters are wrong', (done) => {
+    const spy = jest.spyOn(aesService['_aes'], 'createKey');
+    spy.mockImplementationOnce(() => throwError(() => new Error('Wrong AesService key')));
 
-        aesService.createKey(null, null).subscribe(() => null, err => {
-            expect(err.message).toBe('Wrong AesService key');
-            expect(spy).toHaveBeenCalled();
-            spy.mockRestore();
-            done();
-        });
-    });
-
-    /**
-     * Test if `AesService.createKey().pipe(encryptWithAesKey())` function returns an Observable
-     */
-    test('- `AesService.createKey().pipe(encryptWithAesKey())` function must return an Observable', (done) => {
-        const spy = jest.spyOn(aesService[ '_aes' ], 'createKey');
-        spy.mockImplementationOnce(() =>
-            of({ key: 'aesKey', iv: 'aesIv' })
-        );
-
-        expect(
-            aesService.createKey(password, salt)
-                .pipe(
-                    encryptWithAesKey(null)
-                )
-        ).toBeInstanceOf(Observable);
+    aesService.createKey(null, null).subscribe({
+      next: () => null, error: err => {
+        expect(err.message).toBe('Wrong AesService key');
         expect(spy).toHaveBeenCalled();
         spy.mockRestore();
         done();
+      }
     });
+  });
 
-    /**
-     * Test if `AesService.createKey().pipe(decryptWithAesKey())` function returns an Observable
-     */
-    test('- `AesService.createKey().pipe(decryptWithAesKey())` function must return an Observable', (done) => {
-        const spy = jest.spyOn(aesService[ '_aes' ], 'createKey');
-        spy.mockImplementationOnce(() =>
-            of({ key: 'aesKey', iv: 'aesIv' })
-        );
+  /**
+   * Test if `AesService.createKey().pipe(encryptWithAesKey())` function returns an Observable
+   */
+  test('- `AesService.createKey().pipe(encryptWithAesKey())` function must return an Observable', (done) => {
+    const spy = jest.spyOn(aesService['_aes'], 'createKey');
+    spy.mockImplementationOnce(() =>
+      of({ key: 'aesKey', iv: 'aesIv' })
+    );
 
-        expect(
-            aesService.createKey(password, salt)
-                .pipe(
-                    decryptWithAesKey(null)
-                )
-        ).toBeInstanceOf(Observable);
-        expect(spy).toHaveBeenCalled();
-        spy.mockRestore();
-        done();
-    });
+    expect(
+      aesService.createKey(password, salt)
+        .pipe(
+          encryptWithAesKey(null)
+        )
+    ).toBeInstanceOf(Observable);
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+    done();
+  });
+
+  /**
+   * Test if `AesService.createKey().pipe(decryptWithAesKey())` function returns an Observable
+   */
+  test('- `AesService.createKey().pipe(decryptWithAesKey())` function must return an Observable', (done) => {
+    const spy = jest.spyOn(aesService['_aes'], 'createKey');
+    spy.mockImplementationOnce(() =>
+      of({ key: 'aesKey', iv: 'aesIv' })
+    );
+
+    expect(
+      aesService.createKey(password, salt)
+        .pipe(
+          decryptWithAesKey(null)
+        )
+    ).toBeInstanceOf(Observable);
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+    done();
+  });
 });
